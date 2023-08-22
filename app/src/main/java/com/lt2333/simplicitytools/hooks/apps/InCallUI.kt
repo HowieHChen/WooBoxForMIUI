8b3e1362b96e262fdd5e327d42ade655a601e9f0
howie <howie.c@outlook.com>
2023-08-22 17:58:47 +0800

    修复 隐藏视频彩铃 功能

--- app/src/main/java/com/lt2333/simplicitytools/hooks/apps/InCallUI.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/apps/InCallUI.kt
@@ -2,6 +2,7 @@ package com.lt2333.simplicitytools.hooks.apps
 
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import com.github.kyuubiran.ezxhelper.utils.paramCount
 import com.lt2333.simplicitytools.utils.hasEnable
 import com.lt2333.simplicitytools.utils.xposed.base.AppRegister
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -12,10 +13,9 @@ object InCallUI : AppRegister()   {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         hasEnable("incallui_hide_crbt") {
             findMethod("com.android.incallui.Call") {
-                name == "setPlayingVideoCrbt"
+                name == "setPlayingVideoCrbt" && paramCount == 2
             }.hookBefore {
-                it.args[0] = 0
-                it.args[1] = false
+                it.result = null
             }
         }
     }
